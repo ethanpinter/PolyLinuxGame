1f2c689f102e023f1c76a2ca89b28ac1ba782029
Ethan Pinter <ethanpinter@psu.edu>
2022-06-08 16:02:13 -0400

    add cleaner script and directory randomization

---
+++ cleaner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf $levelDir/dir1
+rm -rf $levelDir/dir2
+rm -rf $levelDir/dir3
+rm -rf $levelDir/dir4
+rm -rf $levelDir/dir5
--- mkdirLevel.sh
+++ mkdirLevel.sh
@@ -13,20 +13,35 @@ echo "Enter the directory to place files REMOVE ME: "
 read levelDir
 echo $USER_HASH
 
-x="makeme"
-y="dir2"
-
 echo "Please create a new directory named $x in the $y location. Use verify.sh to recieve the next password once the directory is created."
 
+dir1Seed=4
+dir2Seed=12
+dir3Seed=8
+dir4Seed=15
+dir5Seed=1
+
+targetFile="makeme"
+
+dir1=sed ''$dir1Seed'!d' dictionary1
+dir2=sed ''$dir2Seed'!d' dictionary3
+dir3=sed ''$dir3Seed'!d' dictionary1
+dir4=sed ''$dir4Seed'!d' dictionary2
+dir5=sed ''$dir5Seed'!d' dictionary2
+
+
+x=$targetFile
+y=$dir1
+
 ## create static directories
-mkdir $levelDir/dir1
-mkdir $levelDir/dir2
-mkdir $levelDir/dir3
-mkdir $levelDir/dir4
-mkdir $levelDir/dir5
+mkdir $levelDir/$dir1
+mkdir $levelDir/$dir2
+mkdir $levelDir/$dir3
+mkdir $levelDir/$dir4
+mkdir $levelDir/$dir5
 
 echo "Done!"
-
+export levelDir
 
 
 $SHELL
