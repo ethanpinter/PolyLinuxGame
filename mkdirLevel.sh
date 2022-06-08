c1e154b844f003c7917459d3b356b42399c60a88
Ethan Pinter <ethanpinter@psu.edu>
2022-06-08 15:38:39 -0400

    tweak

--- mkdirLevel.sh
+++ mkdirLevel.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-levelDir="C:\Users\Ethan\OneDrive - The Pennsylvania State University\Documents\GitHub\PolyLinuxGame"
+
 
 COMB_PASS="pennstate" 
 ## read in user ID and generate hash with combined password
 USER_ID=""
+levelDir=""
 echo "Enter your PSU User ID (xyz1234): "
 read USER_ID
 USER_HASH=$(echo -n $USER_ID$COMB_PASS | md5sum)
+echo "Enter the directory to place files REMOVE ME: "
+read levelDir
 echo $USER_HASH
 
 x="makeme"
@@ -16,11 +19,11 @@ y="dir2"
 echo "Please create a new directory named $x in the $y location. Use verify.sh to recieve the next password once the directory is created."
 
 ## create static directories
-mkdir $levelDir"\dir1"
-mkdir $levelDir"\dir2"
-mkdir $levelDir"\dir3"
-mkdir $levelDir"\dir4"
-mkdir $levelDir"\dir5"
+mkdir $levelDir/dir1
+mkdir $levelDir/dir2
+mkdir $levelDir/dir3
+mkdir $levelDir/dir4
+mkdir $levelDir/dir5
 
 echo "Done!"
 
