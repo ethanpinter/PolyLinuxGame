5cbf287c2c0cfad53f0a3d48b49f677d430ee7c0
Ethan Pinter <ethanpinter@psu.edu>
2022-06-11 20:08:25 -0400

    cleaned up

--- mkdirLevel.sh
+++ mkdirLevel.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-## some notes
-# first user enters their id which is hashed with password
-# next the user enters the directory to place the files: in this case /home/ethan/PolyLinuxGame for testing
-# based on the USER_HASH, 16 pseudorandom directories are created from 3 directories
-# a pseudorandom integer 0-15 is created based on the USER_HASh, this selects the targetDirectory where a directory should be added
-# the user is prompted to create a directory named "makeme" in the target directory
-# once done, the user should run verify.sh to recieve the next level password
-
 declare -a dict1
 declare -a dict2
 declare -a dict3
@@ -26,23 +18,23 @@ declare -a dict15
 declare -a dict16
 declare -a dict17
 
-readarray -t dict1 <airlines.txt
-readarray -t dict2 <appliances.txt
-readarray -t dict3 <beerBrands.txt
-readarray -t dict4 <candy.txt
-readarray -t dict5 <carBrands.txt
-readarray -t dict6 <cheese.txt
-readarray -t dict7 <cities.txt
-readarray -t dict8 <clothingBrands.txt
-readarray -t dict9 <instruments.txt
-readarray -t dict10 <countries.txt
-readarray -t dict11 <dogBreeds.txt
-readarray -t dict12 <fruits.txt
-readarray -t dict13 <operatingSystems.txt
-readarray -t dict14 <seasonings.txt
-readarray -t dict15 <sports.txt
-readarray -t dict16 <fastFood.txt
-readarray -t dict17 <createdDirectoryDictionary.txt
+readarray -t dict1 <dictionaries/airlines.txt
+readarray -t dict2 <dictionaries/appliances.txt
+readarray -t dict3 <dictionaries/beerBrands.txt
+readarray -t dict4 <dictionaries/candy.txt
+readarray -t dict5 <dictionaries/carBrands.txt
+readarray -t dict6 <dictionaries/cheese.txt
+readarray -t dict7 <dictionaries/cities.txt
+readarray -t dict8 <dictionaries/clothingBrands.txt
+readarray -t dict9 <dictionaries/instruments.txt
+readarray -t dict10 <dictionaries/countries.txt
+readarray -t dict11 <dictionaries/dogBreeds.txt
+readarray -t dict12 <dictionaries/fruits.txt
+readarray -t dict13 <dictionaries/operatingSystems.txt
+readarray -t dict14 <dictionaries/seasonings.txt
+readarray -t dict15 <dictionaries/sports.txt
+readarray -t dict16 <dictionaries/fastFood.txt
+readarray -t dict17 <dictionaries/createdDirectoryDictionary.txt
 
 # https://stackoverflow.com/questions/22466704/assign-each-line-of-file-to-be-a-variable
 # Thereafter, you can refer to the lines by number. The first line is "${lines[0]}" and the second is "${lines[1]}", etc.
@@ -54,33 +46,14 @@ levelDir=""
 echo "Enter your PSU User ID (xyz1234): "
 read USER_ID
 USER_HASH=$(echo -n $USER_ID$COMB_PASS | md5sum)
-## write user hash to file
-#processedHash=''
-#for (( i=0; i<${#USER_HASH}; i++ )); do
-#  echo "${USER_HASH:$i:1}"
-#  if [[ $processedHash != *$i* ]]; then
-#    processedHash=$processedHash$i
-#  fi
-#done
-
-
-#echo -n $processedHash > userHash.txt
 echo -n $USER_HASH > userHash.txt
 
-#echo "Enter the directory to place files REMOVE ME: "
-#read levelDir
 levelDir=$(pwd)
-echo "$USER_HASH"
-
-
-
 
 pseudoRANDcapture=$(cut -c 5 userHash.txt)
-pseudoRAND=$(echo "ibase=16; $pseudoRANDcapture" | bc) ## returns a decimal value based on the hex value
+pseudoRAND=$(echo "ibase=16; $pseudoRANDcapture" | bc)
 echo "$pseudoRAND"
 
-## cut must read from a file
-## use cut instead of offset:length 
 loc1=$(cut -c 1 userHash.txt)
 loc2=$(cut -c 2 userHash.txt)
 loc3=$(cut -c 3 userHash.txt)
@@ -101,27 +74,26 @@ loc17=$(cut -c 17 userHash.txt)
 
 
 
-dir1Seed=$(echo "ibase=16; $loc1" | bc) ## can be chosen based on hash value at beginning
+dir1Seed=$(echo "ibase=16; $loc1" | bc) 
 dir2Seed=$(echo "ibase=16; $loc2" | bc)
 dir3Seed=$(echo "ibase=16; $loc3" | bc)
 dir4Seed=$(echo "ibase=16; $loc4" | bc)
 dir5Seed=$(echo "ibase=16; $loc5" | bc)
-dir6Seed=$(echo "ibase=16; $loc6" | bc) ## can be chosen based on hash value at beginning
+dir6Seed=$(echo "ibase=16; $loc6" | bc) 
 dir7Seed=$(echo "ibase=16; $loc7" | bc)
 dir8Seed=$(echo "ibase=16; $loc8" | bc)
 dir9Seed=$(echo "ibase=16; $loc9" | bc)
 dir10Seed=$(echo "ibase=16; $loc10" | bc)
-dir11Seed=$(echo "ibase=16; $loc11" | bc) ## can be chosen based on hash value at beginning
+dir11Seed=$(echo "ibase=16; $loc11" | bc) 
 dir12Seed=$(echo "ibase=16; $loc12" | bc)
 dir13Seed=$(echo "ibase=16; $loc13" | bc)
 dir14Seed=$(echo "ibase=16; $loc14" | bc)
 dir15Seed=$(echo "ibase=16; $loc15" | bc)
 dir16Seed=$(echo "ibase=16; $loc16" | bc)
-createdDirectorySeed=$(echo "ibase=16; $loc17" | bc)
-#targetDirectorySeed=$(echo "ibase=16; ${USER_HASH:4:4}" | bc) ## change based on hash value
 
-## selects words from dictionaries to name directories; variables are strings
-## sed must be replaced
+createdDirectorySeed=$(echo "ibase=16; $loc17" | bc)
+targetDirectorySeed=$(echo "ibase=16; $loc4" | bc)
+
 dir1=$(echo -n "${dict1[$dir1Seed]}")
 dir2=$(echo -n "${dict2[$dir2Seed]}")
 dir3=$(echo -n "${dict3[$dir3Seed]}")
@@ -139,25 +111,8 @@ dir14=$(echo -n "${dict14[$dir14Seed]}")
 dir15=$(echo -n "${dict15[$dir15Seed]}")
 dir16=$(echo -n "${dict16[$dir16Seed]}")
 
-targetDirectory=$(dir$pseudoRAND)
 
-#export $dir1
-#export $dir2
-#export $dir3
-#export $dir4
-#export $dir5
-#export $dir6
-#export $dir7
-#export $dir8
-#export $dir9
-#export $dir10
-#export $dir11
-#export $dir12
-#export $dir13
-#export $dir14
-#export $dir15
-#export $dir16
-
+targetDirectory=
 
 ## create static directories
 mkdir $dir1
