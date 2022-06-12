5b205ce9b96e92b95ed1f1a007e556b3d6e70cfb
Ethan Pinter <ethanpinter@psu.edu>
2022-06-11 22:50:03 -0400

    typo

--- mkdirLevel.sh
+++ mkdirLevel.sh
@@ -39,14 +39,13 @@ readarray -t dict17 <dictionaries/createdDirectoryDictionary.txt
 # https://stackoverflow.com/questions/22466704/assign-each-line-of-file-to-be-a-variable
 # Thereafter, you can refer to the lines by number. The first line is "${lines[0]}" and the second is "${lines[1]}", etc.
 
-COMB_PASS="pennstate" 
 ## read in user ID and generate hash with combined password
 USER_ID=""
 levelDir=""
 echo "Enter your PSU User ID (xyz1234): "
 read USER_ID
-USER_HASH=$(echo -n $USER_ID$COMB_PASS | md5sum)
-echo -n $USER_HASH > userHash.txt
+USER_HASH=$(echo -n "$USER_ID" | md5sum)
+echo -n "$USER_HASH" > userHash.txt
 
 levelDir=$(pwd)
 
--- verify.sh
+++ verify.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pacman -Sy zip 
 USER_ID=""
 levelDir=""
 echo "Enter your PSU User ID (xyz1234): "
@@ -14,7 +13,7 @@ toZip=""
 echo "1 - mkdir Level"
 echo "Enter the number of the level you wish to verify (1): "
 read selectedLevel
-echo $selectedLevel
+echo "$selectedLevel"
 
 case $selectedLevel in
     1) echo "Selected level 1 - mkdir level" && toZip="$levelDir/mkdirlevel";;
@@ -23,12 +22,11 @@ esac
 
 tar -zcvf level.zip $toZip
 userLevelHash=$(md5sum level.zip)
-echo $userLevelHash
+echo "$userLevelHash"
 
-finalHash=$(($userLevelHash + $USER_HASH))
+finalHash=$(echo "$userLevelHash" + "$USER_HASH" | bc)
 echo *
 echo *
 echo *
 echo "Take this hash and input it in the grading system. Be sure to copy it exactly!"
-echo $finalHash  
-
+echo "$finalHash"  
