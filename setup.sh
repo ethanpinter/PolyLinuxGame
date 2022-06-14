2366d1b5cf1dce0f53a2d29b0eb1c1cfb3f4e506
Ethan Pinter <ethanpinter@psu.edu>
2022-06-13 23:05:33 -0400

    finalized setup

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ useradd -p $newPass -m $userName
 cp -r /root/PolyLinuxGame /home/$userName
 su -l $userName
 cd ..
-cd home/$userName || exit
+cd home/$userName/PolyLinuxGame || exit
 
-#./touchLevel
-#./mkdirLevel
-#./cpMvLevel
-#./rmLevel
+./touchLevel
+./mkdirLevel
+./cpMvLevel
+./rmLevel
