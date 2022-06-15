25b32689db146da9819b859d414a64b11bf31bcb
Ethan Pinter <ethanpinter@psu.edu>
2022-06-15 17:47:29 -0400

    no hiddens

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 #trap ' ' 2 20
 rc-update delete hwclock boot
 rc-service hwclock restart
-date +"%Y-%m-%d" > .currentDate.txt
-currentDate=$(head -n 1 .currentDate.txt)
+date +"%Y-%m-%d" > currentDate.txt
+currentDate=$(head -n 1 currentDate.txt)
 USER_ID=""
 echo "Enter your PSU email (xyz1234@psu.edu): "
 read USER_ID
-echo -n "$USER_ID" > .userID.txt
+echo -n "$USER_ID" > userID.txt
 USER_HASH=$(echo -n "$USER_ID" | md5sum)
 echo -n "$USER_HASH" > userHash.txt
 
