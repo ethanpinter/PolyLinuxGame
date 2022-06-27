f4aa618151eb4d03561d298096f3b770f7bf4375
Ethan Pinter <ethanpinter@psu.edu>
2022-06-27 00:53:45 -0400

    test

--- level1.sh
+++ level1.sh
@@ -4,10 +4,11 @@ pseudoRANDcapture=${USER_HASH:2:1}
 pseudoRAND=$(echo -n "ibase=16; $pseudoRANDcapture" | bc)
 secondRANDcapture=${USER_HASH:3:1}
 secondRAND=$(echo -n "ibase=16; $secondRANDcapture" | bc)
-combined="$pseudoRAND,$secondRAND"
+combined=$(echo -n "$pseudoRAND,$secondRAND")
 reference=${masterArray[$combined]}
 createdFile=$(echo -n "$reference")
 targetDirectory="test"
+echo "${masterArray["1,0"]}"
 ## copy verify script into level directory
 mkdir level1/
 cp level1Verify.sh level1/
