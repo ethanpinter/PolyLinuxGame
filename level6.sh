90a8dca605e648b25a482484d7b1985b3299e87d
Ethan Pinter <ethanpinter@psu.edu>
2022-07-08 00:06:39 -0400

    added lvl6 (start of medium levels)

--- level6.sh
+++ level6.sh
@@ -36,47 +36,86 @@ pseudoRAND14=$(echo "ibase=16; $pseudoRANDcapture14" | bc)
 pseudoRAND15=$(echo "ibase=16; $pseudoRANDcapture15" | bc)
 pseudoRAND16=$(echo "ibase=16; $pseudoRANDcapture16" | bc)
 
-## add steps of 16 to each value to ensure no duplicate directory names are created
-## 16 categories containing 16 items
-## change the integer added to keep value in certain range while still being polymorphic
-#pseudoRAND1=$((pseudoRAND1+16)) ## doesnt need changed
+## adds one to handle hash values of 0
+## in this more complex level, the names of the directories are handled differently
+pseudoRAND1=$(( $pseudoRAND1 + 1 ))
 pseudoRAND2=$(( $pseudoRAND2 + 1 ))
-pseudoRAND3=$(( $pseudoRAND3 + 2 ))
-pseudoRAND4=$(( $pseudoRAND4 + 3 ))
-pseudoRAND5=$(( $pseudoRAND5 + 4 ))
-pseudoRAND6=$(( $pseudoRAND6 + 5 ))
-pseudoRAND7=$(( $pseudoRAND7 + 6 ))
-pseudoRAND8=$(( $pseudoRAND8 + 7 ))
-pseudoRAND9=$(( $pseudoRAND9 + 8 ))
-pseudoRAND10=$(( $pseudoRAND10 + 151 ))
-pseudoRAND11=$(( $pseudoRAND11 + 176 ))
-pseudoRAND12=$(( $pseudoRAND12 + 133 ))
-pseudoRAND13=$(( $pseudoRAND13 + 201 ))
-pseudoRAND14=$(( $pseudoRAND14 + 212 ))
-pseudoRAND15=$(( $pseudoRAND15 + 231 ))
-pseudoRAND16=$(( $pseudoRAND16 + 240 ))
+pseudoRAND3=$(( $pseudoRAND3 + 1 ))
+pseudoRAND4=$(( $pseudoRAND4 + 1 ))
+pseudoRAND5=$(( $pseudoRAND5 + 1 ))
+pseudoRAND6=$(( $pseudoRAND6 + 1 ))
+pseudoRAND7=$(( $pseudoRAND7 + 1 ))
+pseudoRAND8=$(( $pseudoRAND8 + 1 ))
+pseudoRAND9=$(( $pseudoRAND9 + 1 ))
+pseudoRAND10=$(( $pseudoRAND10 + 1 ))
+pseudoRAND11=$(( $pseudoRAND11 + 1 ))
+pseudoRAND12=$(( $pseudoRAND12 + 1 ))
+pseudoRAND13=$(( $pseudoRAND13 + 1 ))
+pseudoRAND14=$(( $pseudoRAND14 + 1 ))
+pseudoRAND15=$(( $pseudoRAND15 + 1 ))
+pseudoRAND16=$(( $pseudoRAND16 + 1 ))
 
-## select target directory and name of file to be created by user
-targetDirectory=$(head -n $pseudoRAND6 masterArray.txt | tail -1)
+## based on the first character of the hash, select which set of 16 items should be chosen from masterArray
+case "$pseudoRAND1" in
+    "1") dictSelect=1   # airlines   
+    ;;
+    "2") dictSelect=17  # appliances
+    ;;
+    "3") dictSelect=33  # candy
+    ;;
+    "4") dictSelect=49  # car brands
+    ;;
+    "5") dictSelect=65  # cheese
+    ;;
+    "6") dictSelect=81  # cities
+    ;;
+    "7") dictSelect=97  # clothing brands
+    ;;
+    "8") dictSelect=113 # colleges
+    ;;
+    "9") dictSelect=129 # countries
+    ;;
+    "10") dictSelect=145    # letters
+    ;;
+    "11") dictSelect=161    # dog breeds
+    ;;
+    "12") dictSelect=177    # fast food
+    ;;
+    "13") dictSelect=193    # fruits
+    ;;
+    "14") dictSelect=209    # instruments
+    ;;
+    "15") dictSelect=225    # music artists
+    ;;
+    "16") dictSelect=241    # sports
+    ;;
+esac
+
+
+## endRange adds 16 to get 15 directories the same plus 1 different directory at the tail
+endRange=$(( $dictSelect + 16 ))
+sed -n "$dictSelect","$endRange"p masterArray.txt > dirList.txt
+
+## select name of file to be created by user
 createdFile=$(head -n $pseudoRAND16 masterArray.txt | tail -1)
 
 ## set directories based on pseudorands
-dir1=$(head -n $pseudoRAND1 masterArray.txt | tail -1)
-dir2=$(head -n $pseudoRAND2 masterArray.txt | tail -1)
-dir3=$(head -n $pseudoRAND3 masterArray.txt | tail -1)
-dir4=$(head -n $pseudoRAND4 masterArray.txt | tail -1)
-dir5=$(head -n $pseudoRAND5 masterArray.txt | tail -1)
-dir6=$(head -n $pseudoRAND6 masterArray.txt | tail -1)
-dir7=$(head -n $pseudoRAND7 masterArray.txt | tail -1)
-dir8=$(head -n $pseudoRAND8 masterArray.txt | tail -1)
-dir9=$(head -n $pseudoRAND9 masterArray.txt | tail -1)
-dir10=$(head -n $pseudoRAND10 masterArray.txt | tail -1)
-dir11=$(head -n $pseudoRAND11 masterArray.txt | tail -1)
-dir12=$(head -n $pseudoRAND12 masterArray.txt | tail -1)
-dir13=$(head -n $pseudoRAND13 masterArray.txt | tail -1)
-dir14=$(head -n $pseudoRAND14 masterArray.txt | tail -1)
-dir15=$(head -n $pseudoRAND15 masterArray.txt | tail -1)
-dir16=$(head -n $pseudoRAND16 masterArray.txt | tail -1)
+dir1=$(head -n 1 dirList.txt | tail -1)
+dir2=$(head -n 2 dirList.txt | tail -1)
+dir3=$(head -n 3 dirList.txt | tail -1)
+dir4=$(head -n 4 dirList.txt | tail -1)
+dir5=$(head -n 5 dirList.txt | tail -1)
+dir6=$(head -n 6 dirList.txt | tail -1)
+dir7=$(head -n 7 dirList.txt | tail -1)
+dir8=$(head -n 8 dirList.txt | tail -1)
+dir9=$(head -n 9 dirList.txt | tail -1)
+dir10=$(head -n 10 dirList.txt | tail -1)
+dir11=$(head -n 11 dirList.txt | tail -1)
+dir12=$(head -n 12 dirList.txt | tail -1)
+dir13=$(head -n 13 dirList.txt | tail -1)
+dir14=$(head -n 14 dirList.txt | tail -1)
+dir15=$(head -n 15 dirList.txt | tail -1)
+dir16=$(head -n 17 dirList.txt | tail -1)
 
 ## create level-related directories
 mkdir level6/
@@ -97,13 +136,6 @@ mkdir level6/"$dir14"
 mkdir level6/"$dir15"
 mkdir level6/"$dir16"
 
-
-## gen noise data
-
-
-
-
-
 ## create instruction set
 
 echo "*"
@@ -113,4 +145,4 @@ echo "* Level 6" >> level6/README
 echo "* Create a new file named $createdFile.txt in the directory unlike the others." >> level6/README
 echo "* Once finished, run the verify.sh script." >> level6/README
 
-rm directoryList.txt
+rm dirList.txt
--- testScript.sh
+++ testScript.sh
@@ -1,9 +1,101 @@
 #!/bin/sh
+USER_HASH="4D3260702B1DF76063CE994722D35A4E"
+## grab chars from hash
+pseudoRANDcapture1=${USER_HASH:2:1}
+echo "rand capture: $pseudoRANDcapture1"
+pseudoRANDcapture2=${USER_HASH:3:1}
+pseudoRANDcapture3=${USER_HASH:4:1}
+pseudoRANDcapture4=${USER_HASH:5:1}
+pseudoRANDcapture5=${USER_HASH:6:1}
+pseudoRANDcapture6=${USER_HASH:7:1}
+pseudoRANDcapture7=${USER_HASH:8:1}
+pseudoRANDcapture8=${USER_HASH:9:1}
+pseudoRANDcapture9=${USER_HASH:10:1}
+pseudoRANDcapture10=${USER_HASH:11:1}
+#pseudoRANDcapture11=${USER_HASH:12:1}
+#pseudoRANDcapture12=${USER_HASH:13:1}
+#pseudoRANDcapture13=${USER_HASH:14:1}
+#pseudoRANDcapture14=${USER_HASH:15:1}
+#pseudoRANDcapture15=${USER_HASH:16:1}
+#pseudoRANDcapture16=${USER_HASH:17:1}
 
-file=masterArray.txt
-for i in $(cat $file)
-do
-echo "$i"
-done
+## convert hash character from hex to decimal
+pseudoRAND1=$(echo "ibase=16; $pseudoRANDcapture1" | bc)
+echo "converted to hex: $pseudoRAND1"
+pseudoRAND2=$(echo "ibase=16; $pseudoRANDcapture2" | bc)
+pseudoRAND3=$(echo "ibase=16; $pseudoRANDcapture3" | bc)
+pseudoRAND4=$(echo "ibase=16; $pseudoRANDcapture4" | bc)
+pseudoRAND5=$(echo "ibase=16; $pseudoRANDcapture5" | bc)
+pseudoRAND6=$(echo "ibase=16; $pseudoRANDcapture6" | bc)
+pseudoRAND7=$(echo "ibase=16; $pseudoRANDcapture7" | bc)
+pseudoRAND8=$(echo "ibase=16; $pseudoRANDcapture8" | bc)
+pseudoRAND9=$(echo "ibase=16; $pseudoRANDcapture9" | bc)
+pseudoRAND10=$(echo "ibase=16; $pseudoRANDcapture10" | bc)
+#pseudoRAND11=$(echo "ibase=16; $pseudoRANDcapture11" | bc)
+#pseudoRAND12=$(echo "ibase=16; $pseudoRANDcapture12" | bc)
+#pseudoRAND13=$(echo "ibase=16; $pseudoRANDcapture13" | bc)
+#pseudoRAND14=$(echo "ibase=16; $pseudoRANDcapture14" | bc)
+#pseudoRAND15=$(echo "ibase=16; $pseudoRANDcapture15" | bc)
+#pseudoRAND16=$(echo "ibase=16; $pseudoRANDcapture16" | bc)
+
+## add steps of 16 to each value to ensure no duplicate directory names are created
+## 16 categories containing 16 items
+## change the integer added to keep value in certain range while still being polymorphic
+pseudoRAND1=$(( $pseudoRAND1 + 1 ))
+echo "adding 1 to rand: $pseudoRAND1"
+pseudoRAND2=$(( $pseudoRAND2 + 1 ))
+pseudoRAND3=$(( $pseudoRAND3 + 1 ))
+pseudoRAND4=$(( $pseudoRAND4 + 1 ))
+pseudoRAND5=$(( $pseudoRAND5 + 1 ))
+pseudoRAND6=$(( $pseudoRAND6 + 1 ))
+pseudoRAND7=$(( $pseudoRAND7 + 1 ))
+pseudoRAND8=$(( $pseudoRAND8 + 1 ))
+pseudoRAND9=$(( $pseudoRAND9 + 1 ))
+pseudoRAND10=$(( $pseudoRAND10 + 1 ))
+#pseudoRAND11=$(( $pseudoRAND11 + 176 ))
+#pseudoRAND12=$(( $pseudoRAND12 + 133 ))
+#pseudoRAND13=$(( $pseudoRAND13 + 201 ))
+#pseudoRAND14=$(( $pseudoRAND14 + 212 ))
+#pseudoRAND15=$(( $pseudoRAND15 + 231 ))
+#pseudoRAND16=$(( $pseudoRAND16 + 240 ))
 
 
+case "$pseudoRAND1" in
+    "1") dictSelect=1
+    ;;
+    "2") dictSelect=17
+    ;;
+    "3") dictSelect=33
+    ;;
+    "4") dictSelect=49
+    ;;
+    "5") dictSelect=65
+    ;;
+    "6") dictSelect=81
+    ;;
+    "7") dictSelect=97
+    ;;
+    "8") dictSelect=113
+    ;;
+    "9") dictSelect=129
+    ;;
+    "10") dictSelect=145
+    ;;
+    "11") dictSelect=161
+    ;;
+    "12") dictSelect=177
+    ;;
+    "13") dictSelect=193
+    ;;
+    "14") dictSelect=209
+    ;;
+    "15") dictSelect=225
+    ;;
+    "16") dictSelect=241
+    ;;
+esac
+echo "Dict Select: $dictSelect"
+## dictSelect is the starting line of the dict, should print 16 after that
+## endRange adds 16 to get 16 directories the same plus 1 different directory at the tail
+endRange=$(( $dictSelect + 16 ))
+sed -n "$dictSelect","$endRange"p masterArray.txt > dirs.txt
