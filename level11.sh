fd48425cedee8782f070601debdc922121df09d3
Ethan Pinter <ethanpinter@psu.edu>
2022-07-19 22:34:29 -0400

    added noise to level11

--- level11.sh
+++ level11.sh
@@ -38,7 +38,7 @@ pseudoRAND14=$(echo "ibase=16; $pseudoRANDcapture14" | bc)
 pseudoRAND15=$(echo "ibase=16; $pseudoRANDcapture15" | bc)
 pseudoRAND16=$(echo "ibase=16; $pseudoRANDcapture16" | bc)
 
-## add steps of 16 to each value to ensure no duplicate directory names are created
+## add steps of 16 to each value to ensure no duplicate file names are created
 ## 16 categories containing 16 items
 ## change the integer added to keep value in certain range while still being polymorphic
 pseudoRAND1=$(( $pseudoRAND1 + 1 )) ## ensures value isn't 0
@@ -58,53 +58,118 @@ pseudoRAND14=$(( $pseudoRAND14 + 207 ))
 pseudoRAND15=$(( $pseudoRAND15 + 223 ))
 pseudoRAND16=$(( $pseudoRAND16 + 239 ))
 
-## select target directory and name of file to be created by user
+## select name of file to be created by user
 createdFile=$(head -n $pseudoRAND8 masterArray.txt | tail -1)
-#createdDict=$(head -n $pseudoRAND12 masterArray.txt | tail -1)
 
-## set directories based on pseudorands
-dir1=$(head -n $pseudoRAND1 masterArray.txt | tail -1)
-dir2=$(head -n $pseudoRAND2 masterArray.txt | tail -1)
-dir3=$(head -n $pseudoRAND3 masterArray.txt | tail -1)
-dir4=$(head -n $pseudoRAND4 masterArray.txt | tail -1)
-dir5=$(head -n $pseudoRAND5 masterArray.txt | tail -1)
-dir6=$(head -n $pseudoRAND6 masterArray.txt | tail -1)
-dir7=$(head -n $pseudoRAND7 masterArray.txt | tail -1)
-dir8=$(head -n $pseudoRAND8 masterArray.txt | tail -1)
-dir9=$(head -n $pseudoRAND9 masterArray.txt | tail -1)
-dir10=$(head -n $pseudoRAND10 masterArray.txt | tail -1)
-dir11=$(head -n $pseudoRAND11 masterArray.txt | tail -1)
-dir12=$(head -n $pseudoRAND12 masterArray.txt | tail -1)
-dir13=$(head -n $pseudoRAND13 masterArray.txt | tail -1)
-dir14=$(head -n $pseudoRAND14 masterArray.txt | tail -1)
-dir15=$(head -n $pseudoRAND15 masterArray.txt | tail -1)
-dir16=$(head -n $pseudoRAND16 masterArray.txt | tail -1)
+## set files based on pseudorands
+file1=$(head -n $pseudoRAND1 masterArray.txt | tail -1)
+file2=$(head -n $pseudoRAND2 masterArray.txt | tail -1)
+file3=$(head -n $pseudoRAND3 masterArray.txt | tail -1)
+file4=$(head -n $pseudoRAND4 masterArray.txt | tail -1)
+file5=$(head -n $pseudoRAND5 masterArray.txt | tail -1)
+file6=$(head -n $pseudoRAND6 masterArray.txt | tail -1)
+file7=$(head -n $pseudoRAND7 masterArray.txt | tail -1)
+file8=$(head -n $pseudoRAND8 masterArray.txt | tail -1)
+file9=$(head -n $pseudoRAND9 masterArray.txt | tail -1)
+file10=$(head -n $pseudoRAND10 masterArray.txt | tail -1)
+file11=$(head -n $pseudoRAND11 masterArray.txt | tail -1)
+file12=$(head -n $pseudoRAND12 masterArray.txt | tail -1)
+file13=$(head -n $pseudoRAND13 masterArray.txt | tail -1)
+file14=$(head -n $pseudoRAND14 masterArray.txt | tail -1)
+file15=$(head -n $pseudoRAND15 masterArray.txt | tail -1)
+file16=$(head -n $pseudoRAND16 masterArray.txt | tail -1)
 
-## create level-related directories
+## create level-related files
 mkdir level11/
-mkdir level11/"$dir1"
-mkdir level11/"$dir2"
-mkdir level11/"$dir3"
-mkdir level11/"$dir4"
-mkdir level11/"$dir5"
-mkdir level11/"$dir6"
-mkdir level11/"$dir7"
-mkdir level11/"$dir8"
-mkdir level11/"$dir9"
-mkdir level11/"$dir10"
-mkdir level11/"$dir11"
-mkdir level11/"$dir12"
-mkdir level11/"$dir13"
-mkdir level11/"$dir14"
-mkdir level11/"$dir15"
-mkdir level11/"$dir16"
+touch level11/"$file1"
+touch level11/"$file2"
+touch level11/"$file3"
+touch level11/"$file4"
+touch level11/"$file5"
+touch level11/"$file6"
+touch level11/"$file7"
+touch level11/"$file8"
+
+
+echo "$file9" > "$file1"
+echo "$file10" > "$file1"
+echo "$file11" > "$file1"
+echo "$file12" > "$file1"
+echo "$file13" > "$file1"
+echo "$file14" > "$file1"
+echo "$file15" > "$file1"
+echo "$file16" > "$file1"
+
+echo "$file9" > "$file2"
+echo "$file10" > "$file2"
+echo "$file11" > "$file2"
+echo "$file12" > "$file2"
+echo "$file13" > "$file2"
+echo "$file14" > "$file2"
+echo "$file15" > "$file2"
+echo "$file16" > "$file2"
+
+echo "$file9" > "$file3"
+echo "$file10" > "$file3"
+echo "$file11" > "$file3"
+echo "$file12" > "$file3"
+echo "$file13" > "$file3"
+echo "$file14" > "$file3"
+echo "$file15" > "$file3"
+echo "$file16" > "$file3"
+
+echo "$file9" > "$file4"
+echo "$file10" > "$file4"
+echo "$file11" > "$file4"
+echo "$file12" > "$file4"
+echo "$file13" > "$file4"
+echo "$file14" > "$file4"
+echo "$file15" > "$file4"
+echo "$file16" > "$file4"
+
+echo "$file9" > "$file5"
+echo "$file10" > "$file5"
+echo "$file11" > "$file5"
+echo "$file12" > "$file5"
+echo "$file13" > "$file5"
+echo "$file14" > "$file5"
+echo "$file15" > "$file5"
+echo "$file16" > "$file5"
+
+echo "$file9" > "$file6"
+echo "$file10" > "$file6"
+echo "$file11" > "$file6"
+echo "$file12" > "$file6"
+echo "$file13" > "$file6"
+echo "$file14" > "$file6"
+echo "$file15" > "$file6"
+echo "$file16" > "$file6"
+
+echo "$file9" > "$file7"
+echo "$file10" > "$file7"
+echo "$file11" > "$file7"
+echo "$file12" > "$file7"
+echo "$file13" > "$file7"
+echo "$file14" > "$file7"
+echo "$file15" > "$file7"
+echo "$file16" > "$file7"
+
+echo "$file9" > "$file8"
+echo "$file10" > "$file8"
+echo "$file11" > "$file8"
+echo "$file12" > "$file8"
+echo "$file13" > "$file8"
+echo "$file14" > "$file8"
+echo "$file15" > "$file8"
+echo "$file16" > "$file8"
+
 
 echo "*"
 echo "*"
 echo "*"
 echo "* Level 11" >> level11/README
 echo "* Use the head command to print the first 3 lines of the $createdFile file." >> level11/README
-echo "* Create 3 new directories in the level11 directory named after the lines of the file." >> level11/README
+echo "* Create 3 new directories in the level11 directory named after the lines of the $createdFile file." >> level11/README
 echo "* Once finished, run the verify.sh script." >> level11/README
 
-## potential for reuse of level scripts with different set of directions
+## potential for reuse of level scripts with different set of fileections
